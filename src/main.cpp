0a79e4ae8136b2299d44f04d6f70f501ed3c2dea
ramgmtk <ramgmtk@gmail.com>
2022-10-19 01:04:38 -0700

    Setup main to read input file of vertices

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <fstream>
+#include "structure.h"
 
-int main() {
-    std::cout << "Hello World." << std::endl;
+int main(int argc, char** argv) {
+    std::ifstream infile;
+    //if (argc < 2) //throw std::invalid_argument("./main <inputfile>.txt");
+    //else infile.open(argv[1]);
+    infile.open("input.txt");
+    std::string line;
+    graph g;
+
+    while(std::getline(infile, line, '\n')) g.insert(line);
+    infile.close();
+    g.print();
+
+    system("pause");
     return 0;
 }
